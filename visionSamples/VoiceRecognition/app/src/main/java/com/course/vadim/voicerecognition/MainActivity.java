c7a01532ec363d4b5297747dd22de0eb94d62420
artnovik <a.novikov.dev@gmail.com>
2017-12-15 17:45:42 +0200

    VoiceRecognition BETA

---
+++ visionSamples/VoiceRecognition/app/src/main/java/com/course/vadim/voicerecognition/MainActivity.java
@@ -0,0 +1,80 @@
+package com.course.vadim.voicerecognition;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    private final String LocaleRU = "ru-RU";
+    private final String LocaleUA = "uk-UA";
+    private final String LocaleEN = "en-US";
+
+    private String selectedLocale = "uk-UA";//Locale.getDefault().toString();
+
+    private TextView voiceInput;
+    private TextView speakButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        voiceInput = (TextView) findViewById(R.id.voiceInput);
+        speakButton = (TextView) findViewById(R.id.btnSpeak);
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                askSpeechInput();
+            }
+        });
+
+    }
+
+    // Showing google speech input dialog
+
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, selectedLocale.toString()); //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Hi!\nSpeak something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    // Receiving speech input
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    voiceInput.setText("Output: " + result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+}
